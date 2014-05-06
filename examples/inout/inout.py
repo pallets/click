19e9c6dc136ccf17a226e263407876d245dc7ba5
Armin Ronacher <armin.ronacher@active-4.com>
2014-05-07 00:03:25 +0200

    Updated inout example

--- examples/inout/inout.py
+++ examples/inout/inout.py
@@ -2,12 +2,29 @@ import click
 
 
 @click.command()
-@click.argument('input', type=click.File('rb'))
+@click.argument('input', type=click.File('rb'), nargs=-1)
 @click.argument('output', type=click.File('wb'))
 def cli(input, output):
-    while True:
-        chunk = input.read(1024)
-        if not chunk:
-            break
-        output.write(chunk)
-        output.flush()
+    """This script works similar to the unix `cat` command but it writes
+    into a specific file (which could be the standard output as denoted by
+    the ``-`` sign).
+
+    \b
+    Copy stdin to stdout:
+        inout - -
+
+    \b
+    Copy foo.txt and bar.txt to stdout:
+        inout foo.txt bar.txt -
+
+    \b
+    Write stdin into the file foo.txt
+        intout - foo.txt
+    """
+    for f in input:
+        while True:
+            chunk = f.read(1024)
+            if not chunk:
+                break
+            output.write(chunk)
+            output.flush()
