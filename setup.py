3df2c33b2eaa5c4663d76a3ffb2a17019e792daf
Armin Ronacher <armin.ronacher@active-4.com>
2014-05-23 19:25:46 +0200

    This is 2.0-dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='click',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
-    version='1.0',
+    version='2.0-dev',
     url='http://github.com/mitsuhiko/click',
     packages=['click'],
     description='A simple wrapper around optparse for '
