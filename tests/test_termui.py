a939618ed6096076d46ece7f8a621fe9f25f6d33
Armin Ronacher <armin.ronacher@active-4.com>
2014-08-11 14:23:08 +0200

    Added termui tests

---
+++ tests/test_termui.py
@@ -0,0 +1,14 @@
+import click
+
+
+def test_progressbar_strip_regression(runner, monkeypatch):
+    label = '    padded line'
+
+    @click.command()
+    def cli():
+        with click.progressbar(tuple(range(10)), label=label) as progress:
+            for thing in progress:
+                pass
+
+    monkeypatch.setattr(click._termui_impl, 'isatty', lambda _: True)
+    assert label in runner.invoke(cli, []).output
