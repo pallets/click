3ddc556a9f20267b2c46b5c9ceda6c3d8f26e79b
Frank Hoffmann <frho@mail.de>
2015-04-16 11:52:12 +0200

    Fix type inference for multiple options
    Fix #327

--- tests/test_options.py
+++ tests/test_options.py
@@ -156,6 +156,19 @@ def test_multiple_default_help(runner):
     assert "foo, bar" in result.output
     assert "1, 2" in result.output
 
+def test_multiple_default_type(runner):
+    @click.command()
+    @click.option("--arg1", multiple=True, default=('foo', 'bar'))
+    @click.option("--arg2", multiple=True, default=(1, "a"))
+    def cmd(arg1, arg2):
+        assert all(isinstance(e[0],str) for e in arg1)
+        assert all(isinstance(e[1],str) for e in arg1)
+
+        assert all(isinstance(e[0],int) for e in arg2)
+        assert all(isinstance(e[1],str) for e in arg2)
+
+    result = runner.invoke(cmd, "--arg1 a b --arg1 test 1 --arg2 2 two --arg2 4 four".split())
+    assert not result.exception
 
 def test_nargs_envvar(runner):
     @click.command()
