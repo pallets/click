e172cc08a74ef0b14e3dbfcde138e7622738991d
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-06-21 23:08:48 +0200

    Custom exceptions

--- click/context.py
+++ click/context.py
@@ -4,13 +4,24 @@ class _Stack(object):
 
     @property
     def top(self):
-        return self._stack[-1]
+        try:
+            return self._stack[-1]
+        except IndexError:
+            raise RuntimeError(
+                'Working outside of click context. This means that currently '
+                'no click app is running whose context you could access. Note '
+                'that multiprocessing or similar libraries are not supported '
+                'by click.'
+            )
 
     def push(self, value):
         self._stack.append(value)
 
     def pop(self):
-        return self._stack.pop()
+        try:
+            return self._stack.pop()
+        except RuntimeError:
+            raise RuntimeError('No click context to pop.')
 
 
 class _StackProxy(object):
@@ -20,7 +31,7 @@ class _StackProxy(object):
     def __bool__(self):
         try:
             self. _click_ctx_stack.top
-        except IndexError:
+        except RuntimeError:
             return False
         else:
             return True
