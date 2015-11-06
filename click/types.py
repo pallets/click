ed978a67aa59cf1a93db6ecff65033c459334864
Armin Ronacher <armin.ronacher@active-4.com>
2015-11-06 11:43:50 +0100

    Optionally allow dashes to Path. This fixes #444

--- click/types.py
+++ click/types.py
@@ -343,6 +343,9 @@ class Path(ParamType):
     handle it returns just the filename.  Secondly, it can perform various
     basic checks about what the file or directory should be.
 
+    .. versionchanged:: 6.0
+       `allow_dash` was added.
+
     :param exists: if set to true, the file or directory needs to exist for
                    this value to be valid.  If this is not required and a
                    file does indeed not exist, then all further checks are
@@ -354,6 +357,8 @@ class Path(ParamType):
     :param resolve_path: if this is true, then the path is fully resolved
                          before the value is passed onwards.  This means
                          that it's absolute and symlinks are resolved.
+    :param allow_dash: If this is set to `True`, a single dash to indicate
+                       standard streams is permitted.
     :param type: optionally a string type that should be used to
                  represent the path.  The default is `None` which
                  means the return value will be either bytes or
@@ -364,13 +369,14 @@ class Path(ParamType):
 
     def __init__(self, exists=False, file_okay=True, dir_okay=True,
                  writable=False, readable=True, resolve_path=False,
-                 path_type=None):
+                 allow_dash=False, path_type=None):
         self.exists = exists
         self.file_okay = file_okay
         self.dir_okay = dir_okay
         self.writable = writable
         self.readable = readable
         self.resolve_path = resolve_path
+        self.allow_dash = allow_dash
         self.type = path_type
 
         if self.file_okay and not self.dir_okay:
@@ -383,20 +389,32 @@ class Path(ParamType):
             self.name = 'path'
             self.path_type = 'Path'
 
+    def coerce_path_result(self, rv):
+        if self.type is not None and not isinstance(rv, self.type):
+            if self.type is text_type:
+                rv = rv.decode(get_filesystem_encoding())
+            else:
+                rv = rv.encode(get_filesystem_encoding())
+        return rv
+
     def convert(self, value, param, ctx):
         rv = value
-        if self.resolve_path:
-            rv = os.path.realpath(rv)
 
-        try:
-            st = os.stat(rv)
-        except OSError:
-            if not self.exists:
-                return rv
-            self.fail('%s "%s" does not exist.' % (
-                self.path_type,
-                filename_to_ui(value)
-            ), param, ctx)
+        is_dash = self.file_okay and self.allow_dash and rv in (b'-', '-')
+
+        if not is_dash:
+            if self.resolve_path:
+                rv = os.path.realpath(rv)
+
+            try:
+                st = os.stat(rv)
+            except OSError:
+                if not self.exists:
+                    return self.coerce_path_result(rv)
+                self.fail('%s "%s" does not exist.' % (
+                    self.path_type,
+                    filename_to_ui(value)
+                ), param, ctx)
 
         if not self.file_okay and stat.S_ISREG(st.st_mode):
             self.fail('%s "%s" is a file.' % (
@@ -419,13 +437,7 @@ class Path(ParamType):
                 filename_to_ui(value)
             ), param, ctx)
 
-        if self.type is not None and not isinstance(rv, self.type):
-            if self.type is text_type:
-                rv = rv.decode(get_filesystem_encoding())
-            else:
-                rv = rv.encode(get_filesystem_encoding())
-
-        return rv
+        return self.coerce_path_result(rv)
 
 
 class Tuple(CompositeParamType):
