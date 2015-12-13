11a662df615c302e50a5d387df0803cbe2398d43
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2015-12-13 19:45:16 +0100

    introduce int_types compat to fix python3 type error in _length_hint

--- tests/test_termui.py
+++ tests/test_termui.py
@@ -12,3 +12,31 @@ def test_progressbar_strip_regression(runner, monkeypatch):
 
     monkeypatch.setattr(click._termui_impl, 'isatty', lambda _: True)
     assert label in runner.invoke(cli, []).output
+
+
+def test_progressbar_length_hint(runner, monkeypatch):
+    class Hinted(object):
+        def __init__(self, n):
+            self.items = list(range(n))
+
+        def __length_hint__(self):
+            return len(self.items)
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.items:
+                return self.items.pop()
+            else:
+                raise StopIteration
+
+    @click.command()
+    def cli():
+        with click.progressbar(Hinted(10), label='test') as progress:
+            for thing in progress:
+                pass
+
+    monkeypatch.setattr(click._termui_impl, 'isatty', lambda _: True)
+    result = runner.invoke(cli, [])
+    assert result.exception is None
