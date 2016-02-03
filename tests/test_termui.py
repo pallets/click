00b6ecaa2db463ea94f75564373adc88e63c97c1
her0e1c1 <her0e1c1@gmail.com>
2016-02-03 11:18:18 +0900

    secho print empty string if it takes None

--- tests/test_termui.py
+++ tests/test_termui.py
@@ -12,3 +12,10 @@ def test_progressbar_strip_regression(runner, monkeypatch):
 
     monkeypatch.setattr(click._termui_impl, 'isatty', lambda _: True)
     assert label in runner.invoke(cli, []).output
+
+
+def test_secho(runner):
+    with runner.isolation() as out:
+        click.secho(None, nl=False)
+        bytes = out.getvalue()
+        assert bytes == b''
