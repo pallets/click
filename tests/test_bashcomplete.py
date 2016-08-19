b7ae2a85d396157aa8e24d5ab237ede90f64ebb6
Peter Kelley <peter@knewton.com>
2016-08-19 11:09:33 -0400

    Fix #628: Bash completion for nested commands broken
    When concatenating protected_args and args, protected_args
    should be prepended. Otherwise the arguments are out
    of order and an incorrect subcommand is choosen.

--- tests/test_bashcomplete.py
+++ tests/test_bashcomplete.py
@@ -3,7 +3,18 @@
 import click
 from click._bashcomplete import get_choices
 
-def test_basic():
+
+def test_single_command():
+    @click.command()
+    @click.option('--local-opt')
+    def cli(local_opt):
+        pass
+
+    assert list(get_choices(cli, 'lol', [], '-')) == ['--local-opt']
+    assert list(get_choices(cli, 'lol', [], '')) == []
+
+
+def test_small_chain():
     @click.group()
     @click.option('--global-opt')
     def cli(global_opt):
@@ -18,3 +29,34 @@ def test_basic():
     assert list(get_choices(cli, 'lol', [], '-')) == ['--global-opt']
     assert list(get_choices(cli, 'lol', ['sub'], '')) == []
     assert list(get_choices(cli, 'lol', ['sub'], '-')) == ['--local-opt']
+
+
+def test_long_chain():
+    @click.group('cli')
+    @click.option('--cli-opt')
+    def cli(cli_opt):
+        pass
+
+    @cli.group('asub')
+    @click.option('--asub-opt')
+    def asub(asub_opt):
+        pass
+
+    @asub.group('bsub')
+    @click.option('--bsub-opt')
+    def bsub(bsub_opt):
+        pass
+
+    @bsub.command('csub')
+    @click.option('--csub-opt')
+    def csub(csub_opt):
+        pass
+
+    assert list(get_choices(cli, 'lol', [], '-')) == ['--cli-opt']
+    assert list(get_choices(cli, 'lol', [], '')) == ['asub']
+    assert list(get_choices(cli, 'lol', ['asub'], '-')) == ['--asub-opt']
+    assert list(get_choices(cli, 'lol', ['asub'], '')) == ['bsub']
+    assert list(get_choices(cli, 'lol', ['asub', 'bsub'], '-')) == ['--bsub-opt']
+    assert list(get_choices(cli, 'lol', ['asub', 'bsub'], '')) == ['csub']
+    assert list(get_choices(cli, 'lol', ['asub', 'bsub', 'csub'], '-')) == ['--csub-opt']
+    assert list(get_choices(cli, 'lol', ['asub', 'bsub', 'csub'], '')) == []
