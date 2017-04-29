8435734d6bea19a37cf2dbaee48441cc0a853b6a
Nicholas Wiles <nwiles@google.com>
2017-04-28 18:22:49 -0700

    rollback example

--- examples/bashcompletion/bashcompletion.py
+++ examples/bashcompletion/bashcompletion.py
@@ -1,16 +1,31 @@
 import click
 import os
 
-@click.group(chain=True)
+@click.group()
 def cli():
-	pass
+    pass
+
+def get_env_vars(ctx, args, incomplete):
+    return os.environ.keys()
 
 @cli.command()
-@click.option('-n', is_flag=True)
-def cmd1(n):
-	pass
+@click.argument("envvar", type=click.STRING, autocompletion=get_env_vars)
+def cmd1(envvar):
+    click.echo('Environment variable: %s' % envvar)
+    click.echo('Value: %s' % os.environ[envvar])
 
-@cli.command()
-@click.option('-b', is_flag=True)
-def cmd2(b):
-	pass
+@click.group()
+def group():
+    pass
+
+def list_users(ctx, args, incomplete):
+    # Here you can generate completions dynamically
+    users = ['bob', 'alice']
+    return users
+
+@group.command()
+@click.argument("user", type=click.STRING, autocompletion=list_users)
+def subcmd(user):
+    click.echo('Chosen user is %s' % user)
+
+cli.add_command(group)
