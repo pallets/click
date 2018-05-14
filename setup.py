b1a35cd76f55b8f42c0bf67a3cf69d68aaf773eb
Zachery Bir <zbir@zacbir.net>
2018-05-14 10:31:31 -0400

    Update python_requires values

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
     ],
-    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
 )
