b24209a225de57989cc73e94be33a0f99b6c0851
Ming Zhang <ming.zhang@twosigma.com>
2019-05-31 11:04:04 -0400

    renamed context to environment to reduce confusion

--- examples/complex/complex/commands/cmd_init.py
+++ examples/complex/complex/commands/cmd_init.py
@@ -1,10 +1,10 @@
 import click
-from complex.cli import pass_context
+from complex.cli import pass_environment
 
 
 @click.command('init', short_help='Initializes a repo.')
 @click.argument('path', required=False, type=click.Path(resolve_path=True))
-@pass_context
+@pass_environment
 def cli(ctx, path):
     """Initializes a repository."""
     if path is None:
