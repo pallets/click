3106951b6fc733385f08b2a397621dec756a2989
Nick Cross <ncross@redhat.com>
2019-07-03 21:21:25 +0100

    Correct ZSH completion definitions

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -25,6 +25,14 @@ def test_bash_func_name():
     assert '_COMPLETE_VAR=complete $1' in script
 
 
+def test_zsh_func_name():
+    from click._bashcomplete import get_completion_script
+    script = get_completion_script('foo-bar', '_COMPLETE_VAR', 'zsh').strip()
+    assert script.startswith('#compdef foo-bar')
+    assert 'compdef _foo_bar_completion foo-bar;' in script
+    assert '(( ! $+commands[foo-bar] )) && return 1' in script
+
+
 @pytest.mark.xfail(WIN, reason="Jupyter not tested/supported on Windows")
 def test_is_jupyter_kernel_output():
     class JupyterKernelFakeStream(object):
