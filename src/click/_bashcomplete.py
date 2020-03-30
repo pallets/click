15b4418f630320af3fe9918aaefc0a9270266dfa
Robert Bost <bostrt@users.noreply.github.com>
2020-03-30 15:54:23 -0600

    Minor typo fix in bash completion template

--- src/click/_bashcomplete.py
+++ src/click/_bashcomplete.py
@@ -26,7 +26,7 @@ COMPLETION_SCRIPT_BASH = """
     return 0
 }
 
-%(complete_func)setup() {
+%(complete_func)s_setup() {
     local COMPLETION_OPTIONS=""
     local BASH_VERSION_ARR=(${BASH_VERSION//./ })
     # Only BASH version 4.4 and later have the nosort option.
@@ -38,7 +38,7 @@ COMPLETION_SCRIPT_BASH = """
     complete $COMPLETION_OPTIONS -F %(complete_func)s %(script_names)s
 }
 
-%(complete_func)setup
+%(complete_func)s_setup
 """
 
 COMPLETION_SCRIPT_ZSH = """
