32ddbb11f210dd679439241c6619cad8bcafca67
IamCathal <cathalsocallaghan@gmail.com>
2020-07-06 16:47:51 +0100

    non-chained group invoked without command invokes result callback

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -88,6 +88,25 @@ def test_chaining_with_options(runner):
     assert result.output.splitlines() == ["bdist called 1", "sdist called 2"]
 
 
+@pytest.mark.parametrize(("chain", "expect"), [(False, "None"), (True, "[]")])
+def test_no_command_result_callback(runner, chain, expect):
+    """When a group has ``invoke_without_command=True``, the result
+    callback is always invoked. A regular group invokes it with
+    ``None``, a chained group with ``[]``.
+    """
+
+    @click.group(invoke_without_command=True, chain=chain)
+    def cli():
+        pass
+
+    @cli.resultcallback()
+    def process_result(result):
+        click.echo(str(result), nl=False)
+
+    result = runner.invoke(cli, [])
+    assert result.output == expect
+
+
 def test_chaining_with_arguments(runner):
     @click.group(chain=True)
     def cli():
