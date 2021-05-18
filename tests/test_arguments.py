pallets/click

commit 3d3ea9c64420fd8978a41ce8b4db3ac2d245849c

nargs=-1 works with envvar
nargs > 1 is validated for envvar and default values

Co-authored-by: Rachel Liu <r248liu@uwaterloo.ca>

src/click/parser.py
@@ -183,6 +183,10 @@ class Argument:
                 raise BadArgumentUsage(
                     f"argument {self.dest} takes {self.nargs} values"
                 )
+
+        if self.nargs == -1 and self.obj.envvar is not None:
+            value = None
+
         state.opts[self.dest] = value
         state.order.append(self.obj)
 

tests/test_arguments.py
@@ -18,7 +18,7 @@ def test_nargs_star(runner):
     assert result.output.splitlines() == ["src=foo.txt|bar.txt", "dst=dir"]
 
 
-def test_nargs_default(runner):
+def test_argument_unbounded_nargs_cant_have_default(runner):
     with pytest.raises(TypeError, match="nargs=-1"):
 
         @click.command()
@@ -163,26 +163,31 @@ def test_stdout_default(runner):
     assert result.output == "Foo bar baz\n"
 
 
-def test_nargs_envvar(runner):
+@pytest.mark.parametrize(
+    ("nargs", "value", "code", "output"),
+    [
+        (2, "", 2, "Argument 'arg' takes 2 values but 0 were given."),
+        (2, "a", 2, "Argument 'arg' takes 2 values but 1 was given."),
+        (2, "a b", 0, "len 2"),
+        (2, "a b c", 2, "Argument 'arg' takes 2 values but 3 were given."),
+        (-1, "a b c", 0, "len 3"),
+        (-1, "", 0, "len 0"),
+    ],
+)
+def test_nargs_envvar(runner, nargs, value, code, output):
+    if nargs == -1:
+        param = click.argument("arg", envvar="X", nargs=nargs)
+    else:
+        param = click.option("--arg", envvar="X", nargs=nargs)
+
     @click.command()
-    @click.option("--arg", nargs=2)
+    @param
     def cmd(arg):
-        click.echo("|".join(arg))
+        click.echo(f"len {len(arg)}")
 
-    result = runner.invoke(
-        cmd, [], auto_envvar_prefix="TEST", env={"TEST_ARG": "foo bar"}
-    )
-    assert not result.exception
-    assert result.output == "foo|bar\n"
-
-    @click.command()
-    @click.option("--arg", envvar="X", nargs=2)
-    def cmd(arg):
-        click.echo("|".join(arg))
-
-    result = runner.invoke(cmd, [], env={"X": "foo bar"})
-    assert not result.exception
-    assert result.output == "foo|bar\n"
+    result = runner.invoke(cmd, env={"X": value})
+    assert result.exit_code == code
+    assert output in result.output
 
 
 def test_empty_nargs(runner):
@@ -303,3 +308,23 @@ def test_multiple_param_decls_not_allowed(runner):
         @click.argument("x", click.Choice(["a", "b"]))
         def copy(x):
             click.echo(x)
+
+
+@pytest.mark.parametrize(
+    ("value", "code", "output"),
+    [
+        ((), 2, "Argument 'arg' takes 2 values but 0 were given."),
+        (("a",), 2, "Argument 'arg' takes 2 values but 1 was given."),
+        (("a", "b"), 0, "len 2"),
+        (("a", "b", "c"), 2, "Argument 'arg' takes 2 values but 3 were given."),
+    ],
+)
+def test_nargs_default(runner, value, code, output):
+    @click.command()
+    @click.argument("arg", nargs=2, default=value)
+    def cmd(arg):
+        click.echo(f"len {len(arg)}")
+
+    result = runner.invoke(cmd)
+    assert result.exit_code == code
+    assert output in result.output

src/click/core.py
@@ -1624,11 +1624,25 @@ class Parameter:
 
         if value is None and not ctx.resilient_parsing:
             value = self.get_default(ctx)
+
             if value is not None:
                 ctx.set_parameter_source(self.name, ParameterSource.DEFAULT)
 
         if self.required and self.value_is_missing(value):
             raise MissingParameter(ctx=ctx, param=self)
+
+        # For bounded nargs (!= -1), validate the number of values.
+        if (
+            not ctx.resilient_parsing
+            and self.nargs > 1
+            and self.nargs != len(value)
+            and isinstance(value, (tuple, list))
+        ):
+            were = "was" if len(value) == 1 else "were"
+            ctx.fail(
+                f"Argument {self.name!r} takes {self.nargs} values but"
+                f" {len(value)} {were} given."
+            )
 
         return value
 

commit 1327f44242d7114fb19a03db18d3ba11f7346535

fix argument with nargs=-1 and envvar (#1903)
A change in 3d3ea9c6 broke variadic arguments with an `envvar`
setting: the `process` method replaced `value` with `None`
uncoditionally, thus ignoring the values passed on the command line.
The intention of this was probably to replace the empty tuple (when no
arguments were passed) with `None` so that the value from the
environment would be picked later on. Adjust the condition accordingly
and add a test that covers this.

src/click/parser.py
@@ -233,7 +233,9 @@ class Argument:
                     )
                 )
 
-        if self.nargs == -1 and self.obj.envvar is not None:
+        if self.nargs == -1 and self.obj.envvar is not None and value == ():
+            # Replace empty tuple with None so that the value from the
+            # environment is used.
             value = None
 
         state.opts[self.dest] = value  # type: ignore

tests/test_arguments.py
@@ -194,6 +194,19 @@ def test_nargs_envvar(runner, nargs, value, expect):
         assert result.return_value == expect
 
 
+def test_vararg_envvar_not_used(runner):
+    @click.command()
+    @click.argument("arg", envvar="X", nargs=-1)
+    def cmd(arg):
+        return arg
+
+    value = ("arg1", "arg2")
+
+    result = runner.invoke(cmd, args=value, standalone_mode=False)
+
+    assert result.return_value == value
+
+
 def test_empty_nargs(runner):
     @click.command()
     @click.argument("arg", nargs=-1)