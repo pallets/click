f2e579ab187ca8fdfbe6ce86de08f0e9f62fe4ae
Paul Spangler <paul.spangler@ni.com>
2021-08-09 13:27:39 -0500

    shell completion prioritizes option values over new options
    Allow option values that start with an option prefix to
    complete rather than treating them like new options.
    
    Don't treat count options as needing a value to complete.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,7 @@
 import pytest
 
+import click
+from click.parser import OptionParser
 from click.parser import split_arg_string
 
 
@@ -15,3 +17,16 @@ from click.parser import split_arg_string
 )
 def test_split_arg_string(value, expect):
     assert split_arg_string(value) == expect
+
+
+def test_parser_default_prefixes():
+    parser = OptionParser()
+    assert parser._opt_prefixes == {"-", "--"}
+
+
+def test_parser_collects_prefixes():
+    ctx = click.Context(click.Command("test"))
+    parser = OptionParser(ctx)
+    click.Option("+p", is_flag=True).add_to_parser(parser, ctx)
+    click.Option("!e", is_flag=True).add_to_parser(parser, ctx)
+    assert parser._opt_prefixes == {"-", "--", "+", "!"}
