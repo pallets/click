828277b1e57b39b204171b256fef2e7995c468de
Sagi Shadur <sagi@proxima.co.il>
2022-03-10 13:20:27 +0200

    Add `EnumChoice` parameter type

--- src/click/types.py
+++ src/click/types.py
@@ -1,3 +1,4 @@
+import enum
 import os
 import stat
 import typing as t
@@ -320,6 +321,23 @@ class Choice(ParamType):
             matched = (c for c in str_choices if c.lower().startswith(incomplete))
 
         return [CompletionItem(c) for c in matched]
+
+
+class EnumChoice(Choice):
+    def __init__(self, enum_type: t.Type[enum.Enum], case_sensitive: bool = True):
+        super().__init__(
+            choices=[element.name for element in enum_type],
+            case_sensitive=case_sensitive,
+        )
+        self.enum_type = enum_type
+
+    def convert(
+        self, value: t.Any, param: t.Optional["Parameter"], ctx: t.Optional["Context"]
+    ) -> t.Any:
+        value = super().convert(value=value, param=param, ctx=ctx)
+        if value is None:
+            return None
+        return self.enum_type[value]
 
 
 class DateTime(ParamType):
--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -1,7 +1,18 @@
+import enum
+
 import click
 
 
 CONTEXT_SETTINGS = dict(token_normalize_func=lambda x: x.lower())
+
+
+class MyEnum(enum.Enum):
+    """Dummy enum for unit tests."""
+
+    ONE = "one"
+    TWO = "two"
+    THREE = "three"
+    ONE_ALIAS = ONE
 
 
 def test_option_normalization(runner):
@@ -24,6 +35,16 @@ def test_choice_normalization(runner):
 
     result = runner.invoke(cli, ["--CHOICE", "FOO"])
     assert result.output == "Foo\n"
+
+
+def test_enum_choice_normalization(runner):
+    @click.command(context_settings=CONTEXT_SETTINGS)
+    @click.option("--choice", type=click.EnumChoice(MyEnum))
+    def cli(choice):
+        click.echo(choice)
+
+    result = runner.invoke(cli, ["--CHOICE", "ONE"])
+    assert result.output == "MyEnum.ONE\n"
 
 
 def test_command_normalization(runner):
