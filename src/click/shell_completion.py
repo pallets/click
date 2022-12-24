908e220b87fa9cb565fea3bc757eaa9b3b0140e4
Alexander Regueiro <alexreg@gmail.com>
2022-12-24 17:33:44 +0000

    Fix return value of `shell_completion.add_completion_class` function

--- src/click/shell_completion.py
+++ src/click/shell_completion.py
@@ -398,7 +398,7 @@ _available_shells: t.Dict[str, t.Type[ShellComplete]] = {
 
 def add_completion_class(
     cls: t.Type[ShellComplete], name: t.Optional[str] = None
-) -> None:
+) -> t.Type[ShellComplete]:
     """Register a :class:`ShellComplete` subclass under the given name.
     The name will be provided by the completion instruction environment
     variable during completion.
@@ -412,6 +412,8 @@ def add_completion_class(
         name = cls.name
 
     _available_shells[name] = cls
+
+    return cls
 
 
 def get_completion_class(shell: str) -> t.Optional[t.Type[ShellComplete]]:
