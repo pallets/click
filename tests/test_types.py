pallets/click

commit c8ca29bd3581d3c5e36ea0254f6c8cf0bf6c40c4

use pathlib to resolve symlinks

src/click/types.py
@@ -836,20 +836,11 @@ class Path(ParamType):
 
         if not is_dash:
             if self.resolve_path:
-                # Get the absolute directory containing the path.
-                dir_ = os.path.dirname(os.path.abspath(rv))
+                # os.path.realpath doesn't resolve symlinks on Windows
+                # until Python 3.8. Use pathlib for now.
+                import pathlib
 
-                # Resolve a symlink. realpath on Windows Python < 3.9
-                # doesn't resolve symlinks. This might return a relative
-                # path even if the path to the link is absolute.
-                if os.path.islink(rv):
-                    rv = os.readlink(rv)
-
-                # Join dir_ with the resolved symlink if the resolved
-                # path is relative. This will make it relative to the
-                # original containing directory.
-                if not os.path.isabs(rv):
-                    rv = os.path.join(dir_, rv)
+                rv = os.fsdecode(pathlib.Path(rv).resolve())
 
             try:
                 st = os.stat(rv)

tests/test_types.py
@@ -2,7 +2,7 @@ import os.path
 import pathlib
 
 import pytest
-from conftest import check_symlink_impl
+from conftest import symlinks_supported
 
 import click
 
@@ -104,37 +104,27 @@ def test_path_type(runner, cls, expect):
     assert result.return_value == expect
 
 
-@pytest.mark.skipif(not check_symlink_impl(), reason="symlink not allowed on device")
-@pytest.mark.parametrize(
-    ("sym_file", "abs_fun"),
-    [
-        (("relative_symlink",), os.path.basename),
-        (("test", "absolute_symlink"), lambda x: x),
-    ],
+@pytest.mark.skipif(
+    not symlinks_supported, reason="The current OS or FS doesn't support symlinks."
 )
-def test_symlink_resolution(tmpdir, sym_file, abs_fun):
-    """This test ensures symlinks are properly resolved by click"""
-    tempdir = str(tmpdir)
-    real_path = os.path.join(tempdir, "test_file")
-    sym_path = os.path.join(tempdir, *sym_file)
+def test_path_resolve_symlink(tmp_path, runner):
+    test_file = tmp_path / "file"
+    test_file_str = os.fsdecode(test_file)
+    test_file.write_text("")
 
-    # create dirs and files
-    os.makedirs(os.path.join(tempdir, "test"), exist_ok=True)
-    open(real_path, "w").close()
-    os.symlink(abs_fun(real_path), sym_path)
+    path_type = click.Path(resolve_path=True)
+    param = click.Argument(["a"], type=path_type)
+    ctx = click.Context(click.Command("cli", params=[param]))
 
-    # test
-    ctx = click.Context(click.Command("do_stuff"))
-    rv = click.Path(resolve_path=True).convert(sym_path, None, ctx)
+    test_dir = tmp_path / "dir"
+    test_dir.mkdir()
 
-    # os.readlink prepends path prefixes to absolute
-    # links in windows.
-    # https://docs.microsoft.com/en-us/windows/win32/
-    # ... fileio/naming-a-file#win32-file-namespaces
-    #
-    # Here we strip win32 path prefix from the resolved path
-    rv_drive, rv_path = os.path.splitdrive(rv)
-    stripped_rv_drive = rv_drive.split(os.path.sep)[-1]
-    rv = os.path.join(stripped_rv_drive, rv_path)
+    abs_link = test_dir / "abs"
+    abs_link.symlink_to(test_file)
+    abs_rv = path_type.convert(os.fsdecode(abs_link), param, ctx)
+    assert abs_rv == test_file_str
 
-    assert rv == real_path
+    rel_link = test_dir / "rel"
+    rel_link.symlink_to(pathlib.Path("..") / "file")
+    rel_rv = path_type.convert(os.fsdecode(rel_link), param, ctx)
+    assert rel_rv == test_file_str

tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import tempfile
 
 import pytest
@@ -12,20 +11,17 @@ def runner(request):
     return CliRunner()
 
 
-def check_symlink_impl():
-    """This function checks if using symlinks is allowed
-    on the host machine"""
-    tempdir = tempfile.mkdtemp(prefix="click-")
-    test_pth = os.path.join(tempdir, "check_sym_impl")
-    sym_pth = os.path.join(tempdir, "link")
-    open(test_pth, "w").close()
-    rv = True
-    try:
-        os.symlink(test_pth, sym_pth)
-    except (NotImplementedError, OSError):
-        # Creating symlinks on Windows require elevated access.
-        # OSError is thrown if the function is called without it.
-        rv = False
-    finally:
-        shutil.rmtree(tempdir, ignore_errors=True)
-    return rv
+def _check_symlinks_supported():
+    with tempfile.TemporaryDirectory(prefix="click-pytest-") as tempdir:
+        target = os.path.join(tempdir, "target")
+        open(target, "w").close()
+        link = os.path.join(tempdir, "link")
+
+        try:
+            os.symlink(target, link)
+            return True
+        except OSError:
+            return False
+
+
+symlinks_supported = _check_symlinks_supported()

commit a7c5f5b5f025084132b255abc950ee538abe16bb

Test more of click.Path interface
The existing tests missed the combination of resolve_path=True and
converting a value of type bytes, for example.

This bug was introduced by c8ca29bd3581d3c5e36ea0254f6c8cf0bf6c40c4 (#2094)

I've replaced os.fsdecode(<Path>) with os.fspath(<Path>), because calling
os.fsdecode on a type whose __fspath__ returns str is unnecessary since
os.fsdecode returns str unchanged.

src/click/types.py
@@ -851,7 +851,7 @@ class Path(ParamType):
                 # until Python 3.8. Use pathlib for now.
                 import pathlib
 
-                rv = os.fsdecode(pathlib.Path(rv).resolve())
+                rv = os.fspath(pathlib.Path(os.fsdecode(rv)).resolve())
 
             try:
                 st = os.stat(rv)

tests/test_types.py
@@ -85,23 +85,28 @@ def test_cast_multi_default(runner, nargs, multiple, default, expect):
 
 
 @pytest.mark.parametrize(
-    ("cls", "expect"),
+    ("type", "value", "expect"),
     [
-        (None, "a/b/c.txt"),
-        (str, "a/b/c.txt"),
-        (bytes, b"a/b/c.txt"),
-        (pathlib.Path, pathlib.Path("a", "b", "c.txt")),
+        (click.Path(resolve_path=True), "foo/bar", os.path.realpath("foo/bar")),
+        (click.Path(resolve_path=True), b"foo/bar", os.path.realpath("foo/bar")),
+        (
+            click.Path(resolve_path=True),
+            pathlib.Path("foo/bar"),
+            os.path.realpath("foo/bar"),
+        ),
+        (click.Path(), "foo/bar", "foo/bar"),
+        (click.Path(), b"foo/bar", b"foo/bar"),
+        (click.Path(path_type=None), "foo/bar", "foo/bar"),
+        (click.Path(path_type=None), b"foo/bar", b"foo/bar"),
+        (click.Path(path_type=str), "foo/bar", "foo/bar"),
+        (click.Path(path_type=str), b"foo/bar", "foo/bar"),
+        (click.Path(path_type=bytes), "foo/bar", b"foo/bar"),
+        (click.Path(path_type=bytes), b"foo/bar", b"foo/bar"),
+        (click.Path(path_type=pathlib.Path), "foo/bar", pathlib.Path("foo/bar")),
     ],
 )
-def test_path_type(runner, cls, expect):
-    cli = click.Command(
-        "cli",
-        params=[click.Argument(["p"], type=click.Path(path_type=cls))],
-        callback=lambda p: p,
-    )
-    result = runner.invoke(cli, ["a/b/c.txt"], standalone_mode=False)
-    assert result.exception is None
-    assert result.return_value == expect
+def test_path(type, value, expect):
+    assert type.convert(value, None, None) == expect
 
 
 @pytest.mark.skipif(