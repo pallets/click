7675de427840ce4db16e9059e219b295002cfd36
Eric Platon <__ic@tutamail.com>
2026-05-05 17:27:26 +0900

    Remove unnecessary comment
    The code looks clear from the context.

--- src/click/_termui_impl.py
+++ src/click/_termui_impl.py
@@ -175,7 +175,6 @@ class ProgressBar(t.Generic[V]):
             if t > 0:
                 return "{d}{day_label} {h:02}:{m:02}:{s:02}".format(
                     d=t,
-                    # TRANSLATORS: The 'd' stands for 'day'.
                     day_label=_("d"),
                     h=hours,
                     m=minutes,
