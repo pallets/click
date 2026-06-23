748a34d01e0f4f2e5f75e293c1dd8480b16c2068
Vizonex <VizonexBusiness@gmail.com>
2026-06-22 22:30:42 -0500

    optimize split_arg_string with extend(...) instead of a for loop system.

--- src/click/shell_completion.py
+++ src/click/shell_completion.py
@@ -505,8 +505,7 @@ def split_arg_string(string: str) -> list[str]:
     out = []
 
     try:
-        for token in lex:
-            out.append(token)
+        out.extend(lex)
     except ValueError:
         # Raised when end-of-string is reached in an invalid state. Use
         # the partial token as-is. The quote or escape character is in
