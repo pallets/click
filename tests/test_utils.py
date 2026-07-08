182944f90a0103b87570dbc5a55eff873ca9a4c2
Edward G <edward.g2013@gmail.com>
2026-07-07 22:29:39 -0700

    Make confirm, prompt, style files.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -60,102 +60,12 @@ def test_unset_sentinel():
     assert value not in real_values
 
 
-@pytest.mark.parametrize(
-    ("styles", "ref"),
-    [
-        ({"fg": "black"}, "\x1b[30mx y\x1b[0m"),
-        ({"fg": "red"}, "\x1b[31mx y\x1b[0m"),
-        ({"fg": "green"}, "\x1b[32mx y\x1b[0m"),
-        ({"fg": "yellow"}, "\x1b[33mx y\x1b[0m"),
-        ({"fg": "blue"}, "\x1b[34mx y\x1b[0m"),
-        ({"fg": "magenta"}, "\x1b[35mx y\x1b[0m"),
-        ({"fg": "cyan"}, "\x1b[36mx y\x1b[0m"),
-        ({"fg": "white"}, "\x1b[37mx y\x1b[0m"),
-        ({"bg": "black"}, "\x1b[40mx y\x1b[0m"),
-        ({"bg": "red"}, "\x1b[41mx y\x1b[0m"),
-        ({"bg": "green"}, "\x1b[42mx y\x1b[0m"),
-        ({"bg": "yellow"}, "\x1b[43mx y\x1b[0m"),
-        ({"bg": "blue"}, "\x1b[44mx y\x1b[0m"),
-        ({"bg": "magenta"}, "\x1b[45mx y\x1b[0m"),
-        ({"bg": "cyan"}, "\x1b[46mx y\x1b[0m"),
-        ({"bg": "white"}, "\x1b[47mx y\x1b[0m"),
-        ({"bg": 91}, "\x1b[48;5;91mx y\x1b[0m"),
-        ({"bg": (135, 0, 175)}, "\x1b[48;2;135;0;175mx y\x1b[0m"),
-        ({"bold": True}, "\x1b[1mx y\x1b[0m"),
-        ({"dim": True}, "\x1b[2mx y\x1b[0m"),
-        ({"underline": True}, "\x1b[4mx y\x1b[0m"),
-        ({"overline": True}, "\x1b[53mx y\x1b[0m"),
-        ({"italic": True}, "\x1b[3mx y\x1b[0m"),
-        ({"blink": True}, "\x1b[5mx y\x1b[0m"),
-        ({"reverse": True}, "\x1b[7mx y\x1b[0m"),
-        ({"strikethrough": True}, "\x1b[9mx y\x1b[0m"),
-        ({"bold": False}, "\x1b[22mx y\x1b[0m"),
-        ({"dim": False}, "\x1b[22mx y\x1b[0m"),
-        ({"underline": False}, "\x1b[24mx y\x1b[0m"),
-        ({"overline": False}, "\x1b[55mx y\x1b[0m"),
-        ({"italic": False}, "\x1b[23mx y\x1b[0m"),
-        ({"blink": False}, "\x1b[25mx y\x1b[0m"),
-        ({"reverse": False}, "\x1b[27mx y\x1b[0m"),
-        ({"strikethrough": False}, "\x1b[29mx y\x1b[0m"),
-        ({"fg": "black", "reset": False}, "\x1b[30mx y"),
-    ],
-)
-def test_styling(styles, ref):
-    assert click.style("x y", **styles) == ref
-    assert click.unstyle(ref) == "x y"
-
-
-@pytest.mark.parametrize(("text", "expect"), [("\x1b[?25lx y\x1b[?25h", "x y")])
-def test_unstyle_other_ansi(text, expect):
-    assert click.unstyle(text) == expect
-
-
 def test_filename_formatting():
     assert click.format_filename(b"foo.txt") == "foo.txt"
     assert click.format_filename(b"/x/foo.txt") == "/x/foo.txt"
     assert click.format_filename("/x/foo.txt") == "/x/foo.txt"
     assert click.format_filename("/x/foo.txt", shorten=True) == "foo.txt"
     assert click.format_filename("/x/\ufffd.txt", shorten=True) == "�.txt"
-
-
-def test_prompts(runner):
-    @click.command()
-    def test():
-        if click.confirm("Foo"):
-            click.echo("yes!")
-        else:
-            click.echo("no :(")
-
-    result = runner.invoke(test, input="y\n")
-    assert not result.exception
-    assert result.output == "Foo [y/N]: y\nyes!\n"
-
-    result = runner.invoke(test, input="\n")
-    assert not result.exception
-    assert result.output == "Foo [y/N]: \nno :(\n"
-
-    result = runner.invoke(test, input="n\n")
-    assert not result.exception
-    assert result.output == "Foo [y/N]: n\nno :(\n"
-
-    @click.command()
-    def test_no():
-        if click.confirm("Foo", default=True):
-            click.echo("yes!")
-        else:
-            click.echo("no :(")
-
-    result = runner.invoke(test_no, input="y\n")
-    assert not result.exception
-    assert result.output == "Foo [Y/n]: y\nyes!\n"
-
-    result = runner.invoke(test_no, input="\n")
-    assert not result.exception
-    assert result.output == "Foo [Y/n]: \nyes!\n"
-
-    result = runner.invoke(test_no, input="n\n")
-    assert not result.exception
-    assert result.output == "Foo [Y/n]: n\nno :(\n"
 
 
 def test_confirm_repeat(runner):
